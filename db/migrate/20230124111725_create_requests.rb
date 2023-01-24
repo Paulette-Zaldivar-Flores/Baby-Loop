b38723f925535c28af1614eb94a62f4fbf6ea30b
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-01-24 20:18:35 +0900

    Creating models

---
+++ db/migrate/20230124111725_create_requests.rb
@@ -0,0 +1,13 @@
+class CreateRequests < ActiveRecord::Migration[7.0]
+  def change
+    create_table :requests do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.integer :status
+      t.references :offer, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_092127) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_111725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.string "item_name"
+    t.string "category"
+    t.text "description"
+    t.integer "rate"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "status"
+    t.bigint "offer_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_requests_on_offer_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +45,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_092127) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offers", "users"
+  add_foreign_key "requests", "offers"
+  add_foreign_key "requests", "users"
 end
