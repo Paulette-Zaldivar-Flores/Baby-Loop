83a210ff0317685c9eeb875a86477dff0b2e2bf0
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-01-26 00:55:14 +0900

    Created necessary new and create models for Offers and Requests

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,9 +1,28 @@
 class OffersController < ApplicationController
-  before_action :set_offer, only: %i[show]
+  before_action :set_offer, only: %i[show create]
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    if @offer.save
+      redirect_to offers_path, message: 'Offer created.'
+    else
+      render 'offers/show'
+    end
+  end
+
 
   def show; end
 
+  private
   def set_offer
     @offer = Offer.find(params[:id])
+  end
+
+  def offer_params
+    params.require(:offer).permit(:item_name, :category, :rate, :description)
   end
 end
