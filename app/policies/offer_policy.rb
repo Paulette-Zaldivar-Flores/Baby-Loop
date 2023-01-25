f1a8a3e2555e9e0503f416667e66cc14111ab27a
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-01-25 21:46:17 +0900

    Creating pundit for Offer methods

---
+++ app/policies/offer_policy.rb
@@ -0,0 +1,37 @@
+class OfferPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  ######## We currently don't have an edit option yet for our offers
+  # def edit?
+  #   update?
+  # end
+
+  # def update?
+  #   ecord.user == user
+  # end
+
+  # def destroy?
+  #   record.user == user
+  # end
+end
