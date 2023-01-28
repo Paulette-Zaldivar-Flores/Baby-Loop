db9c3df52ae93495cbdfd967249922fc30d08b6c
Soares Kim <kimuedasoares@gmail.com>
2023-01-28 14:30:57 +0900

    installstimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -10,11 +10,11 @@ export { application }
 
 // ./packs/application.js
 // import { Application } from 'stimulus'
-import { definitionsFromContext } from 'stimulus/webpack-helpers'
+// import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
 // const application = Application.start()
-const context = require.context('../controllers', true, /\.js$/)
-application.load(definitionsFromContext(context))
+// const context = require.context('../controllers', true, /\.js$/)
+// application.load(definitionsFromContext(context))
 
 // import Flatpickr
 // import Flatpickr from 'stimulus-flatpickr'
