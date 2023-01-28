5fba91199c1f36d09e14d6fa17e03c4aea50d7cf
Soares Kim <kimuedasoares@gmail.com>
2023-01-28 13:39:48 +0900

    updatebundle

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -17,10 +17,10 @@ const context = require.context('../controllers', true, /\.js$/)
 application.load(definitionsFromContext(context))
 
 // import Flatpickr
-import Flatpickr from 'stimulus-flatpickr'
+// import Flatpickr from 'stimulus-flatpickr'
 
 // Import style for flatpickr
-require("flatpickr/dist/flatpickr.css")
+// require("flatpickr/dist/flatpickr.css")
 
 // Manually register Flatpickr as a stimulus controller
-application.register('flatpickr', Flatpickr)
+// application.register('flatpickr', Flatpickr)
