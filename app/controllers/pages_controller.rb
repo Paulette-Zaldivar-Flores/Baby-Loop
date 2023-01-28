ada000f659ed639f155c3e89fa043641d631c3f6
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-01-28 15:34:13 +0900

    completed show css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if user_signed_in?
     @user = current_user
     @requests = @user.requests
+  else
+    @requests = []
+    end
   end
 end
