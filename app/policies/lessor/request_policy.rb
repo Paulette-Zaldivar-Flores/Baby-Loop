cc4300e8e81e6f8b544ba2b1537f7ebfa17dd916
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-02-02 19:41:00 +0900

    Changed policy

--- app/policies/lessor/request_policy.rb
+++ app/policies/lessor/request_policy.rb
@@ -1,10 +1,8 @@
 class Lessor::RequestPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-    policy_scope([:lessor, Request])
-    authorize([:lessor, @request])
+    def resolve
+      scope.all
+    end
   end
 end
