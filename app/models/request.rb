04472f487491153f52a29a2324509e9838c4da0f
Soares Kim <kimuedasoares@gmail.com>
2023-02-04 13:06:53 +0900

    changesindisplaypicture

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,6 @@ class Request < ApplicationRecord
   belongs_to :offer
   belongs_to :user
   enum :status, { pending: 0, accepted: 1, rejected: 2 }
+  validates :end_date, presence: true
+  validates :start_date, presence: true
 end
