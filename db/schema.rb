84eb7cd0392104346eb25cc85d8dee31f37253a8
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-03-28 09:54:54 +0900

    adjusted pipeline issues and banner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_110506) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_110034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,13 +47,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_110506) do
     t.string "category"
     t.text "description"
     t.integer "rate"
-    t.integer "rating"
-    t.string "address"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
+    t.integer "rating"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
